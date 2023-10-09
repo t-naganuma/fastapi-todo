05ab2fcbbb44f88cfec9c90538956f4eb9ed2a50
t-naganuma <takuya.naganuma@gmail.com>
2023-10-09 19:13:43 +0900

    schema request

--- api/routers/done.py
+++ api/routers/done.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
 
+import api.schemas.task as task_schema
+
 router = APIRouter()
 
-@router.put("/tasks/{task_id}/done")
-async def mark_task_as_done():
-  pass
+@router.put("/tasks/{task_id}/done", response_model=None)
+async def mark_task_as_done(task_id: int):
+  return
 
-@router.delete("/tasks/{task_id}/done")
-async def unmark_task_as_done():
-  pass
+@router.delete("/tasks/{task_id}/done", response_model=None)
+async def unmark_task_as_done(task_id: int):
+  return
